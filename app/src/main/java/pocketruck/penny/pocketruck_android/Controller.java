190ff3be5318e6b4745906dd1b6b06fe2917d11b
pennyyi97 <pennyyi97@gmail.com>
2019-06-02 21:53:17 +0900

    ENH:controller추가

--- app/src/main/java/pocketruck/penny/pocketruck_android/Controller.java
+++ app/src/main/java/pocketruck/penny/pocketruck_android/Controller.java
@@ -2,6 +2,8 @@ package pocketruck.penny.pocketruck_android;
 
 import android.app.Application;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import pocketruck.penny.pocketruck_android.api.RetrofitAPI;
 
 import retrofit2.Retrofit;
@@ -29,6 +31,11 @@ public class Controller {
 
     public void retrofitService(){
         if (retrofitAPI == null){//이거 안하면 널포인트 익셉션일어남 왜그러는지 나도 모르겠어.....이거 해도 널포인트 익셉션이 일어나네,,,,죽고싶다,,,
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient.Builder client = new OkHttpClient.Builder();
+            client.interceptors().add(logging);
+
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(BaseURL)
                     .addConverterFactory(GsonConverterFactory.create())//파싱등록
@@ -41,6 +48,6 @@ public class Controller {
 
 }
 /*
-* https://newland435.tistory.com/25
-* 참고했음
-* */
+ * https://newland435.tistory.com/25
+ * 참고했음
+ * */
